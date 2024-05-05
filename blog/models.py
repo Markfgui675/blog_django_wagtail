af2122e38862463cebaf1f3a092f837448839cf8
Marcos Fellipe Guimarães <marcosfgui2006@gmail.com>
2024-05-05 17:08:06 -0300

    models

--- blog/models.py
+++ blog/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 from wagtail.models import Page
 from wagtail.fields import RichTextField
 from wagtail.admin.panels import FieldPanel
@@ -10,9 +11,11 @@ class Home(Page): ...
 class Blog(Page):
     subtitle = models.TextField(max_length=500, blank=True)
     body = RichTextField(blank=False)
+    author = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
 
     content_panels = [
         FieldPanel('title'),
         FieldPanel('subtitle'),
-        FieldPanel('body')
+        FieldPanel('body'),
+        FieldPanel('author')
     ]
